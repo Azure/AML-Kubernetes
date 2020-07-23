dd3b11540fd5fb8c091b04ce5de510d64772dcc9
Cloga Chen <cloga0216@gmail.com>
2020-07-23 09:03:47 +0800

    update sample
    add private sdk installation step
    replace joblib in sklearn with standalone  joblib module

--- sample_notebooks/002 single node job/sklearn/sklearn-nb.py
+++ sample_notebooks/002 single node job/sklearn/sklearn-nb.py
@@ -1,7 +1,8 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.externals import joblib
+# from sklearn.externals import joblib
+import joblib
 import os
 from azureml.core.run import Run
 run = Run.get_context()
@@ -10,7 +11,7 @@ run = Run.get_context()
 os.makedirs('./outputs', exist_ok=True)
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
-for i in range(1000000000000000):
+for i in range(10):
     gnb = GaussianNB()
     model = gnb.fit(X_train, y_train)
 y_pred = model.predict(X_test)
