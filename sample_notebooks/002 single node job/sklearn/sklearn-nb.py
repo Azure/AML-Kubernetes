6ea90aed0b796fb9c5a556921d7499e3f5dc5c65
MuziSakura <1786710691@qq.com>
2020-07-29 23:28:42 +0800

    Remove estimator samples

--- sample_notebooks/002 single node job/sklearn/sklearn-nb.py
+++ sample_notebooks/002 single node job/sklearn/sklearn-nb.py
@@ -11,7 +11,7 @@ run = Run.get_context()
 os.makedirs('./outputs', exist_ok=True)
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
-for i in range(10):
+for i in range(10000000):
     gnb = GaussianNB()
     model = gnb.fit(X_train, y_train)
 y_pred = model.predict(X_test)
