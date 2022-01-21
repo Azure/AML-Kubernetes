a82872dbfa92c0a029e3e404066fab213ecae1cd
zetia <zetia@microsoft.com>
2022-01-21 05:39:36 +0000

    refine code

--- .github/amlarc-tool.sh
+++ .github/amlarc-tool.sh
@@ -637,7 +637,6 @@ ICM_XML_TEMPLATE='<?xml version="1.0" encoding="UTF-8"?>
     echo "code: $ret" 
     echo "Response: $temp_file"
     xmlstarlet fo --indent-tab --omit-decl $temp_file
-    echo "code2: $?"
     return $ret
 }
 
