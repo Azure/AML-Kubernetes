261b0681f779ba91311d448cb583181b46952c35
zetia <zetia@microsoft.com>
2022-01-22 03:21:12 +0000

    inference test

--- .github/amlarc-tool.sh
+++ .github/amlarc-tool.sh
@@ -382,8 +382,6 @@ delete_workspace(){
 
 # run cli test job
 run_cli_job(){
-    #set -e
-    
     JOB_YML="${1:-examples/training/simple-train-cli/job.yml}"
     SET_ARGS="${@:2}"
     if [ "$SET_ARGS" != "" ]; then
@@ -433,8 +431,6 @@ install_jupyter_dependency(){
 
 # run jupyter test
 run_jupyter_test(){
-    set -e
-
     JOB_SPEC="${1:-examples/training/simple-train-sdk/img-classification-training.ipynb}"
     JOB_DIR=$(dirname $JOB_SPEC)
     JOB_FILE=$(basename $JOB_SPEC)
@@ -456,8 +452,6 @@ run_jupyter_test(){
 
 # run python test
 run_py_test(){
-    set -e
-
     JOB_SPEC="${1:-python-sdk/workflows/train/fastai/mnist/job.py}"
     JOB_DIR=$(dirname $JOB_SPEC)
     JOB_FILE=$(basename $$JOB_SPEC)
