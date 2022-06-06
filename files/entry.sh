3f8ab86b640ed636edea0c465f5aee0afb67bfcd
Mingda Jia <mingj@microsoft.com>
2022-06-06 17:49:17 +0800

    recover

--- files/entry.sh
+++ files/entry.sh
@@ -5,5 +5,4 @@ AZ_CONNECTED_K8S_VERSION='1.2.8'
 
 az extension add --name connectedk8s --version $AZ_CONNECTED_K8S_VERSION
 az extension add --name k8s-extension --version $AZ_K8S_EXTENSION_VERSION
-
 python deploy.py
