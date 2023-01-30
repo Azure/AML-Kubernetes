6fb7d7180596890446ff859e63662cbc31adcef9
Zeliang Tian <zetia@microsoft.com>
2023-01-30 21:42:59 +0800

    fix jupyter to 7.4.9

--- .github/amlarc-tool.sh
+++ .github/amlarc-tool.sh
@@ -582,7 +582,9 @@ EOF
 }
 
 install_jupyter_dependency(){
-    pip install jupyter==7.4.9
+    pip list || true
+    pip install jupyter-client==7.4.9
+    pip install jupyter==1.0.0
     pip install notebook 
     ipython kernel install --name "amlarc" --user
     pip install matplotlib numpy scikit-learn==0.22.1 numpy joblib glob2
